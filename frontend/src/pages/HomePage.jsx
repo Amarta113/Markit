7ef9895debdb05945b0f27e4ef57d3fc5476cc72
Amarta113 <amartawaghani@gmail.com>
2026-05-05 20:49:14 +0500

    setup hero section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom'
 import Header from '../components/Layout/Header.jsx'
+import Hero from '../components/Route/Hero/Hero.jsx'
 
 export default function HomePage () {
     return (
         <div>
-            <Header activeHeading={1}/>            
+            <Header activeHeading={1}/>
+            <Hero/>            
         </div>
     )
 }
