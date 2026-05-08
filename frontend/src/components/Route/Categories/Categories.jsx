715abed652301a861872e3b444ace07952cecf3d
Amarta113 <amartawaghani@gmail.com>
2026-05-08 21:44:50 +0500

    categories setup

---
+++ frontend/src/components/Route/Categories/Categories.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from '../../../styles/styles'
+import {brandingData} from '../../../static/data.jsx'
+
+export default function Categories(){
+    return (
+        <div className={`${styles.section} hidden sm:block`}>
+            <div className={`branding my-12 flex justify-between w-full shadow-sm bg-white p-5 rounded-md`}>
+                {brandingData?.map((i) => (
+                    <div className='flex items-start' key={i.id}>
+                        {i.icon}
+                        <div className='px-3'>
+                          <h3 className='font-bold text-sm md:text-base'>
+                            {i.title}
+                          </h3>
+                          <p className='text-sm md:text-sm'>{i.Description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+)}
