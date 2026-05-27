41455335494a0c81566113bb25cf2df5d58f398d
Amarta113 <amartawaghani@gmail.com>
2026-05-27 22:05:37 +0500

    added cart & wishlist stores

--- frontend/redux/store.js
+++ frontend/redux/store.js
@@ -1,9 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {userReducer} from "./reducer/user.js";
+import { cartReducer } from './reducer/cartReducer.js';
+import { wishlistReducer } from './reducer/wishlistReducer.js';
 
 const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer,
+    wishlist: wishlistReducer
   },
 }); 
 
