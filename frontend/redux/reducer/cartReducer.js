eb7b53dca8d5ca2c78588f13bc87f93807c2a9ea
Amarta113 <amartawaghani@gmail.com>
2026-05-27 09:33:10 +0500

    initialized cart reducer

---
+++ frontend/redux/reducer/cartReducer.js
@@ -0,0 +1,32 @@
+import {createReducer} from '@reduxjs/toolkit'
+import {addToCart, addToCartAction, removeFromCartAction} from '../actions/cartActions'
+
+
+const initialState = {
+    cart: localStorage.getItem("cartItems")? JSON.parse(localStorage.getItem("cartItems")) : []
+}
+
+export const cartReducer = createReducer(initialState, builder => {
+    builder
+    .addCase(addToCartAction, (state, action) => {
+        const item = action.payload;
+        const isItemExist = state?.cart?.find(it => it?._id === item?._id)
+        if(isItemExist){
+            return {
+                ...state,
+                cart: state.cart.map(i => (i._id === isItemExist._id? item : i))
+            }
+        } else {
+            return {
+                ...state,
+                cart: [...state.cart, item]
+            }
+        }
+    })
+    .addCase(removeFromCartAction ,(state, action) => {
+        return {
+            ...state,
+            cart: state.cart.filter(i => i._id !== action.payload)
+        }
+    })
+})
