d2f1de49d505cca4373fb9cf1d2927de8a91d251
Amarta113 <amartawaghani@gmail.com>
2026-05-27 22:02:35 +0500

    added navbar mobile responsiveness

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -3,13 +3,13 @@ import styles from '../../styles/styles'
 import { navItems } from '../../static/data'
 import { Link } from 'react-router-dom'
 
-export default function Navbar({active}) {
+export default function Navbar({active, mobile}) {
     return (
-        <div className={`${styles.normalFlex}`}>
+        <div className={`${mobile ? "flex flex-col" : styles.normalFlex}`}>
             {navItems && navItems.map((i, index) => (
-                <div className='flex'>
+                <div className="flex" key={index}>
                     <Link to={i.url}
-                    className={`${active === index + 1? "text-black" : "text-gray-500"} font-[500] px-6 cursor-pointer`}>
+                    className={`${active === index + 1 ? "text-black" : "text-gray-500"} font-[500] px-6 py-3 cursor-pointer`}>
                         {i.title}
                     </Link>
                 </div>
