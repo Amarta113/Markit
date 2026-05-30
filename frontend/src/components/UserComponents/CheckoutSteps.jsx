fd4cff7fb5c9a23e6ed14596e7ea777cc77367e5
Amarta113 <amartawaghani@gmail.com>
2026-05-30 14:56:50 +0500

    Initialized checkoutSteps component

---
+++ frontend/src/components/UserComponents/CheckoutSteps.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import styles from "../../styles/styles";
+import {useSelector} from "react-redux"
+import ShoppingInfo from '../UserComponents/ShoppingInfo'
+import {useState} from "react"
+import {toast} from "react-toastify"
+import axios from 'axios'
+
+
+function CheckoutSteps() {
+    return <></>
+}
+
+export default CheckoutSteps;
