15665db02990893a3e5905ea8a6863c7ab23519a
Amarta113 <amartawaghani@gmail.com>
2026-05-30 14:57:46 +0500

    initialized component

---
+++ frontend/src/components/UserComponents/Payment.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Payment = () => {
+    return (
+        <div>
+            <h1>
+                Default payment page
+            </h1>
+        </div>
+    )
+}
+
+export default Payment
---
+++ frontend/src/components/UserComponents/ShoppingInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ShoppingInfo () {
+    return (
+        <div>
+            <h1>Shopping info</h1>
+        </div>
+    )
+}
