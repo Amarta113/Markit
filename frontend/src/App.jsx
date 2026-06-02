d277e28c5d559e3289170a23980d481f80be37ad
Amarta113 <amartawaghani@gmail.com>
2026-06-02 22:12:10 +0500

    added checkout, payment and product routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,24 +5,30 @@ import {ToastContainer, Bounce } from 'react-toastify';
 import { useEffect } from 'react';
 import store from '../redux/store.js';
 import { loadUser } from '../redux/actions/user.js';
+import ProductDetailsPage from "./pages/ProductDetailsPage.jsx";
+import CheckoutPage from './pages/CheckoutPage.jsx';
+import PaymentPage from './pages/PaymentPage.jsx'
+import OrderSuccessPage from './pages/OrderSuccessPage.jsx'
 
 export default function App () {
   useEffect(() => {
    store.dispatch(loadUser())
   }, [])
-  return (
-    <BrowserRouter>
+  return (   
+      <BrowserRouter>
       <Routes>
-        <Route path='/Home' element={<HomePage/>} />
+        <Route path='/' element={<HomePage/>} />
         <Route path='/login' element={<LoginPage/>} />
         <Route path='/sign-up' element={<SignupPage/>} />
         <Route path='/activation/:activation_token' element={<ActivationPage/>} />
         <Route path="/products" element={<ProductsPage/>}/>
+        <Route path="/products/:name" element={<ProductDetailsPage/>}/>
         <Route path='/best-selling' element={<BestSellingPage/>}/>
         <Route path='/events' element={<EventsPage/>}/>
         <Route path='/faq' element={<FaqPage/>}/>
-
-
+        <Route path='/checkout' element={<CheckoutPage/>}/>
+        <Route path='/payment' element={<PaymentPage/>}/>
+        <Route path="/order/success/:id" element={<OrderSuccessPage/>}/>
       </Routes>
       <ToastContainer
           position="top-right"
@@ -37,8 +43,6 @@ export default function App () {
           theme="dark"
           transition={Bounce}
           />
-    </BrowserRouter>
-    
-  )
+    </BrowserRouter>)
 }
 
