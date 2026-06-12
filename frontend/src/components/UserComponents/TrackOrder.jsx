f4dc30442e9cf43574ec93a68416cb12c52dc556
Amarta113 <amartawaghani@gmail.com>
2026-06-12 21:49:23 +0500

    added Track Order component

---
+++ frontend/src/components/UserComponents/TrackOrder.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import { MdTrackChanges } from "react-icons/md";
+import { DataGrid } from "@mui/x-data-grid";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+
+const TrackOrder = () => {
+    const orders = [
+        {
+            _id: "746hvbfbhfbrtr28820221",
+            orderItems: [
+                {name: "Iphone 14 pro max"}
+            ],
+            totalPrice: 120,
+            orderStatus: "Processing"
+        }
+    ]
+  const columns = [
+    {
+      field: "id",
+      headerName: "Order ID",
+      minWidth: 150,
+      flex: 0.7,
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: params =>
+        params.row.status === "Delivered" ? "greenColor" : "redColor",
+    },
+    {
+      field: "itemsQty",
+      headerName: "Items Qty",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.8,
+    },
+    {
+      field: "actions",
+      headerName: "",
+      sortable: false,
+      minWidth: 150,
+      flex: 1,
+      renderCell: params => (
+        <Link to={`/user/track-order/${params.id}`}>
+          <Button>
+            <MdTrackChanges size={20} />
+          </Button>
+        </Link>
+      ),
+    },
+  ];
+  const row = [];
+
+  orders &&
+    orders.forEach(item => {
+      row.push({
+        id: item._id,
+        itemsQty: item?.cart?.length,
+        total: "US$" + item?.totalPrice,
+        status: item?.status,
+      });
+    });
+
+  return (
+    <div className="pl-8 pt-1 flex flex-col  min-h-[200px] max-h-[600px]">
+      <DataGrid
+        rows={row}
+        columns={columns}
+        pageSizeOptions={[10]}
+        initialState={{
+          pagination: { paginationModel: { pageSize: 10, page: 0 } },
+        }}
+        disableRowSelectionOnClick
+        sx={{ flexGrow: 1 }}
+      />
+    </div>
+  );
+}
+
+
+export default TrackOrder
