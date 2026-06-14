02b831d42156418cdb15fcc0af96d28a75e50b36
Amarta113 <amartawaghani@gmail.com>
2026-06-14 22:21:59 +0500

    feat: add logout route to userRouter for user session management

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { register, activateUser, loginUser, loadUser} from '../controller/userController.js';
+import { register, activateUser, loginUser, loadUser, logoutUser} from '../controller/userController.js';
 import upload from '../multer.js';
 import { catchAsyncError } from '../middleware/catchAsyncError.js';
 import { isAuthenticated } from '../middleware/auth.js';
@@ -9,5 +9,5 @@ router.post("/register", upload.single("avatar"), register)
 router.post("/activation", activateUser)
 router.post("/login-user", loginUser);
 router.get("/get-user", isAuthenticated, loadUser)
-
+router.get("/logout-user", isAuthenticated, logoutUser)
 export default router;
