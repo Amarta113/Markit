6b972225f2c66b3e9b26a147dfe4a5e84cceb672
Amarta113 <amartawaghani@gmail.com>
2026-06-16 22:03:36 +0500

    added seller path

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,23 @@ import cookieParser from "cookie-parser";
 import express from "express"
 import { errorMiddleware } from "./middleware/error.js";
 import userRouter from "./routes/userRouter.js"
+import shopRouter from "./routes/shopRouter.js"
 import connectDb from "./db/Database.js";
+import dns from "dns";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "http://localhost:5173",
     credentials: true
 }))
 app.use('/api/v1/user', userRouter)
+app.use('/api/v1/seller', shopRouter)
+
 app.get('/', (req, res) => {res.json({success: true, message: "server is running"})})
 app.use(errorMiddleware)
 
