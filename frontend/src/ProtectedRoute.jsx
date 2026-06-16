90aa770983d0abec4f2c23b51940b18ad783d651
Amarta113 <amartawaghani@gmail.com>
2026-06-16 22:05:08 +0500

    add ProtectedRoute component for authentication handling

--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom"
 
 export default function ProtectedRoute ({isAuthenticated, children}){
     if(!isAuthenticated){
-        return <Navigate to="/login-user" replace/>
+        return <Navigate to="/login" replace />
     }
     return children
 }
