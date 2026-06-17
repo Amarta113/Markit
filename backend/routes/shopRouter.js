57a68dff9e551469e33a04aa02316961dddfa902
Amarta113 <amartawaghani@gmail.com>
2026-06-17 17:31:30 +0500

    replace isAuthenticated middleware with isSeller for seller routes

--- backend/routes/shopRouter.js
+++ backend/routes/shopRouter.js
@@ -2,12 +2,12 @@ import express from 'express'
 import { createShop, activateSeller, loginSeller, loadSeller, logoutSeller} from '../controller/shopController.js';
 import upload from '../multer.js';
 import { catchAsyncError } from '../middleware/catchAsyncError.js';
-import { isAuthenticated } from '../middleware/auth.js';
+import { isSeller } from '../middleware/auth.js';
 
 const shopRouter = express.Router()
 shopRouter.post("/create-seller", upload.single("avatar"), createShop)
 shopRouter.post("/seller-account/activation", activateSeller)
 shopRouter.post("/login-seller", loginSeller);
-shopRouter.get("/get-seller", isAuthenticated, loadSeller)
-shopRouter.get("/logout-seller", isAuthenticated, logoutSeller)
+shopRouter.get("/get-seller", isSeller, loadSeller)
+shopRouter.get("/logout-seller", isSeller, logoutSeller)
 export default shopRouter;
