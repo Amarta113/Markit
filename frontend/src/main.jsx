2b516c87cc9ced966ad3180f8fa503bc72c63f8a
Amarta113 <amartawaghani@gmail.com>
2026-06-19 20:04:51 +0500

    integrate BrowserRouter in main.jsx, update server configuration with backend_url, and rename file input in ShopCreate component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import "react-toastify/dist/ReactToastify.css";
 import App from './App.jsx'
 import { Provider } from 'react-redux';
 import store from '../redux/store.js';
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
     <Provider store={store}>
-        <App/>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </Provider>
 )
