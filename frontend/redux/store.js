d54ac8415511cd746519b679586206eb42367cb3
Amarta113 <amartawaghani@gmail.com>
2026-06-19 19:59:39 +0500

    added seller reducer

--- frontend/redux/store.js
+++ frontend/redux/store.js
@@ -2,11 +2,14 @@ import {configureStore} from '@reduxjs/toolkit';
 import {userReducer} from "./reducer/user.js";
 import { cartReducer } from './reducer/cartReducer.js';
 import { wishlistReducer } from './reducer/wishlistReducer.js';
+import { sellerReducer } from './reducer/sellerReducer.js';
+
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     cart: cartReducer,
+    seller: sellerReducer,
     wishlist: wishlistReducer
   },
 }); 
