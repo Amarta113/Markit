ad7d17875bf6999d965c1691fa6b52c0c8933ce5
Amarta113 <amartawaghani@gmail.com>
2026-06-20 22:16:57 +0500

    updated folder structure

--- frontend/src/SellerProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom"
-
-export default function SellerProtectedRoute ({isSeller, children}){
-    if(!isSeller){
-        return <Navigate to={`/`} replace />
-    }
-    return children
-}
