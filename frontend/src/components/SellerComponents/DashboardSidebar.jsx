529cb2e62fb831c5203e82475f49098a9cc94820
Amarta113 <amartawaghani@gmail.com>
2026-06-22 22:25:01 +0500

    implement DashboardSidebar component with layout and styling

--- frontend/src/components/SellerComponents/DashboardSidebar.jsx
+++ frontend/src/components/SellerComponents/DashboardSidebar.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { RxDashboard } from "react-icons/rx";
 
 const DashboardSidebar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='w-full h-[100vh] bg-white shadow-sm sticky top-0 left-0 z-10'>
+            {/* single item */}
+            <div className="w-full flex items-center p-4">
+                <Link>
+                    <RxDashboard
+                        size={30}
+                        color={`${activeHeading === 1 ? 'crimson' : '#555'}`} />
+                    <h5 className={`display-bloakc-800px hidden pl-2 text-[18px] font-[400] ${activeHeading === 1 ? "text-[crimson]" : "text-[#555]"}`}>
+                        Dashboard
+                    </h5>
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default DashboardSidebar
