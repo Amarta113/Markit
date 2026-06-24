8641fa3713d93e6a76e06af944c82af061ae5ff1
Amarta113 <amartawaghani@gmail.com>
2026-06-24 21:46:59 +0500

    improve style by adding margin

--- frontend/src/components/SellerComponents/DashboardHeader.jsx
+++ frontend/src/components/SellerComponents/DashboardHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { MdOutlineLocalOffer } from "react-icons/md";
 import { Link } from 'react-router-dom'
-import logoImg from '../../assets/markit-log.png'
+import logoImg from '../../assets/markit-logo.jpg'
 import { AiOutlineGift } from "react-icons/ai";
 import { FiPackage, FiShoppingBag } from "react-icons/fi";
 import { BiMessageSquareDetail } from "react-icons/bi";
@@ -11,14 +11,13 @@ const DashboardHeader = () => {
   const { seller } = useSelector((state) => state.seller)
   return (
     <div className='w-full h-[80px] bg-white shadow sticky top-0 left-0 z-30 flex item-center justify-between px-4'>
-      <div>
+      <div className='800px:block hidden'>
         <Link to="/dashboard">
           <img src={logoImg} alt='logo' className='w-[140px] bg-transparent' />
         </Link>
       </div>
       <div className='flex items-center'>
-
-        <div className="flex items-center">
+        <div className="flex items-center mr-4">
           <Link to={'/dashboard/coupons'}>
             <AiOutlineGift color='#555' size={30} className='mx-5 cursor-pointer display-block-800px hidden' />
           </Link>
