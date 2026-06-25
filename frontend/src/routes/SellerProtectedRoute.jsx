f2f2acd4a0fc1c99cf310806a7180e2e377a6d62
Amarta113 <amartawaghani@gmail.com>
2026-06-25 10:45:28 +0500

    format code

--- frontend/src/routes/SellerProtectedRoute.jsx
+++ frontend/src/routes/SellerProtectedRoute.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom"
+import Loader from "../components/Layout/Loader";
+
 
 export default function SellerProtectedRoute({children }) {
     const { isSeller, isLoading } = useSelector(state => state.seller);
-
-    if (isLoading === true) return <Loader />;
-    else {
+    if (isLoading === true) {
+        return (<Loader />)
+    } else {
         if (!isSeller) return <Navigate to={`/shop-login`} replace />;
     }
 
