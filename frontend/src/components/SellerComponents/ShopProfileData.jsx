3eda10265ae12e55f7e0e536d437c352f6cc9b7e
Amarta113 <amartawaghani@gmail.com>
2026-06-25 12:51:06 +0500

    added isOwner prop to ShopProfileData component

--- frontend/src/components/SellerComponents/ShopProfileData.jsx
+++ frontend/src/components/SellerComponents/ShopProfileData.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const ShopProfileData = () => {
+const ShopProfileData = ({isOwner}) => {
   return (
-    <div>
-      
+    <div className='w-full'>
+      <div className="flex w-full items-center justify-between">
+          <div className="flex item-center">
+            <h5 className={`font-[600] text-[20px] text-red-500`}>
+              Shop Products
+            </h5>
+          </div>
+      </div>
     </div>
   )
 }
