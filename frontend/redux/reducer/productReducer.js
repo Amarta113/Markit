41b55bafe4affefc9bdad355d256f58089736455
Amarta113 <amartawaghani@gmail.com>
2026-06-28 13:20:24 +0500

    feat: add productReducer for managing product loading state and success status

---
+++ frontend/redux/reducer/productReducer.js
@@ -0,0 +1,24 @@
+import { createReducer } from "@reduxjs/toolkit";
+import { createProduct } from "../../../backend/controller/productController";
+
+const initialState = {
+    isLoading: true
+}
+
+export const productReducer = createReducer(
+    initialState, builder => {
+        builder
+        .addCase(createProduct.pending, (state) => {
+            state.isLoading = true;
+        })
+        .addCase(createProduct.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.success = true;
+            state.product = action.payload;
+        })
+        .addCase(createProduct.rejected, (state, action) => {
+            state.isLoading = false;
+            state.success = false;            
+        })
+    }
+)
