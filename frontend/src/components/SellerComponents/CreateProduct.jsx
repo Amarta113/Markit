5f74f355d3c25049546b4c6755e29e2ae956d3e1
Amarta113 <amartawaghani@gmail.com>
2026-06-30 10:43:43 +0500

    add product creation functionality with form handling and notifications

--- frontend/src/components/SellerComponents/CreateProduct.jsx
+++ frontend/src/components/SellerComponents/CreateProduct.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { AiOutlinePlusCircle } from "react-icons/ai";
+import { createProduct } from '../../../redux/actions/productActions';
+import { toast } from 'react-toastify'
 
 function CreateProduct() {
-    const { seller} = useSelector((state) => state.seller)
+    const { seller } = useSelector((state) => state.seller)
+    const { success, error } = useSelector((state) => state.products)
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
@@ -18,8 +21,33 @@ function CreateProduct() {
     const [stock, setStock] = useState()
 
     const handleSubmit = (e) => {
-        e.preventDefault()   
+        e.preventDefault()
+        const newForm = new FormData()
+        images.forEach((image) => {
+            newForm.append("images", image)
+        })
+        newForm.append("name", name)
+        newForm.append("description", description)
+        newForm.append("category", category)
+        newForm.append("tags", tags)
+        newForm.append("originalPrice", originalPrice)
+        newForm.append("discountPrice", discountPrice)
+        newForm.append("stock", stock)
+        newForm.append("shopId", seller._id)
+
+        dispatch(createProduct(newForm))
     }
+
+    useEffect(() => {
+        if (error) {
+            toast.error(error)
+        }
+        if (success) {
+            toast.success("Product created successfully.")
+            navigate('/dashboard')
+            window.location.reload()
+        }
+    }, [dispatch, error, success])
     const handleImageChange = (e) => {
         e.preventDefault()
         let files = Array.from(e.target.files)
@@ -30,76 +58,80 @@ function CreateProduct() {
             <h5 className='text-[30px] font-Poppins text-center'>
                 Create Product
             </h5>
-            {/* create product form*/}
+            {/* Create product form */}
             <form onSubmit={handleSubmit}>
                 <br />
                 <div>
                     <label htmlFor="pb-2">
                         Name <span className="text-red-500">*</span>
                     </label>
-                    <input type="text" name='name' value={name} 
-                    className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
-                    onChange={(e) => setName(e.target.value)}
-                    placeholder='Enter your product name...' />
+                    <input type="text" name='name' value={name}
+                        className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder='Enter your product name...' />
                 </div>
                 <br />
                 <div>
                     <label htmlFor="pb-2">
                         Description <span className="text-red-500">*</span>
                     </label>
-                    <input type="text" name='description' value={description} 
-                    className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
-                    onChange={(e) => setDescription(e.target.value)}
-                    placeholder='Enter your product description...' />
+                    <textarea
+                        type="text"
+                        name='description'
+                        value={description}
+                        className='mt-2 appearance-none block w-full pt-2 px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
+                        onChange={(e) => setDescription(e.target.value)}
+                        placeholder='Enter your product description...'>
+                    </textarea>
                 </div>
                 <br />
                 <div>
                     <label htmlFor="pb-2">
                         Category <span className="text-red-500">*</span>
                     </label>
-                    <select 
-                    className='w-full mt-2 border h-[35px] roudned-[5px]' 
-                    name="category" value={category}
-                    onChange={(e) => setCategory(e.target.value)}>
+                    <select
+                        className='w-full mt-2 border h-[35px] roudned-[5px]'
+                        name="category" value={category}
+                        onChange={(e) => setCategory(e.target.value)}>
                         <option value="Choose a category">Choose a category</option>
-                    {
-                        categoriesData && categoriesData.map((i) => (
-                            <option value={i.title} key={i.title}>
-                                {i.title}
-                            </option>
-                        ))
-                    }
+                        {
+                            categoriesData && categoriesData.map((i) => (
+                                <option value={i.title} key={i.title}>
+                                    {i.title}
+                                </option>
+                            ))
+                        }
                     </select>
                 </div>
                 <br />
                 <div>
                     <label htmlFor="pb-2">
-                        Tags 
+                        Tags
                     </label>
-                    <input type="text" name='tags' value={tags} 
-                    className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
-                    onChange={(e) => setTags(e.target.value)}
-                    placeholder='Enter your product tags...' />
+                    <input type="text" name='tags' value={tags}
+                        className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
+                        onChange={(e) => setTags(e.target.value)}
+                        placeholder='Enter your product tags...' />
                 </div>
                 <br />
                 <div>
                     <label htmlFor="pb-2">
-                        Original Price 
+                        Original Price
                     </label>
-                    <input type="number" name='price' value={originalPrice} 
-                    className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
-                    onChange={(e) => setOriginalPrice(e.target.value)}
-                    placeholder='Enter your Original Price...' />
+                    <input type="number" name='price' value={originalPrice}
+                        className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
+                        onChange={(e) => setOriginalPrice(e.target.value)}
+                        placeholder='Enter your Original Price...' />
                 </div>
                 <br />
                 <div>
                     <label htmlFor="pb-2">
-                       Price (with Discount)<span className="text-red-500">*</span>
+                        Price (with Discount)<span className="text-red-500">*</span>
                     </label>
-                    <input type="number" name='discount-price' value={discountPrice} 
-                    className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
-                    onChange={(e) => setDiscountPrice(e.target.value)}
-                    placeholder='Enter your Price with discount...' />
+                    <input type="number" name='discount-price' value={discountPrice}
+                        className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
+                        onChange={(e) => setDiscountPrice(e.target.value)}
+                        placeholder='Enter your Price with discount...' />
                 </div>
                 <br />
                 <div>
@@ -107,9 +139,9 @@ function CreateProduct() {
                         Product Stock <span className="text-red-500">*</span>
                     </label>
                     <input type="number" name='stock' value={stock}
-                    className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
-                    onChange={(e) => setStock(e.target.value)}
-                    placeholder='Enter your product stock...' />
+                        className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
+                        onChange={(e) => setStock(e.target.value)}
+                        placeholder='Enter your product stock...' />
                 </div>
                 <br />
                 <div>
@@ -117,30 +149,30 @@ function CreateProduct() {
                         Upload Images <span className="text-red-500">*</span>
                     </label>
                     <input type="file" name="" id="upload"
-                    className='hidden' 
-                    multiple
-                    onChange={handleImageChange}/>
-                   <div className="w-full flex items-center flex-wrap">
-                     <label htmlFor="upload">
-                        <AiOutlinePlusCircle
-                        size={30}
-                        className='mt-3'
-                        color='#555'
+                        className='hidden'
+                        multiple
+                        onChange={handleImageChange} />
+                    <div className="w-full flex items-center flex-wrap">
+                        <label htmlFor="upload">
+                            <AiOutlinePlusCircle
+                                size={30}
+                                className='mt-3'
+                                color='#555'
+                            />
+                        </label>
+                        {
+                            images && images.map((i) => (
+                                <img src={URL.createObjectURL(i)} key={i} alt=""
+                                    className='h-[120px] w-[120px] object-cover m-2' />
+                            ))
+                        }
+                    </div>
+                    <br />
+                    <div>
+                        <input type="submit" value="Create"
+                            className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
                         />
-                    </label>
-                    {
-                        images && images.map((i) => (
-                            <img src={URL.createObjectURL(i)} key={i} alt="" 
-                            className='h-[120px] w-[120px] object-cover m-2'/>
-                        ))
-                    }
-                   </div>
-                   <br />
-                   <div>
-                    <input type="submit" value="Create" 
-                    className='mt-2 appearance-none block w-full px-3 h-[35px] border border-gray-300 rounded-[3px] placeholder-gray-400 focus:outline-none focus:ring-blue-500 sm:text-sm'
-                    />
-                   </div>
+                    </div>
                 </div>
             </form>
         </div>
