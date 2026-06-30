f17a1a464029d950752716bc99ff5ae54cbc9fe2
Amarta113 <amartawaghani@gmail.com>
2026-06-30 21:39:50 +0500

    added delete-shop-product route to handle product deletion

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import upload from '../multer.js'
 import {isAuthenticated, isSeller} from '../middleware/auth.js'
-import { createProduct, getAllProductsShop } from '../controller/productController.js'
+import { createProduct, getAllProductsShop, deleteProduct } from '../controller/productController.js'
 
 const productRouter = express.Router()
 
 productRouter.post('/create-product', upload.array("images"), createProduct)
 productRouter.get('/get-all-products-shop/:id', getAllProductsShop)
+productRouter.delete('/delete-shop-product/:id', isSeller, deleteProduct)
 export default productRouter
