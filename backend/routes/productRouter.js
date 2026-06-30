a6b1bd522cdf9e7d59024fbedfd6e55b17219904
Amarta113 <amartawaghani@gmail.com>
2026-06-30 20:10:37 +0500

    added getAllProducts router path

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import upload from '../multer.js'
 import {isAuthenticated, isSeller} from '../middleware/auth.js'
-import { createProduct } from '../controller/productController.js'
+import { createProduct, getAllProductsShop } from '../controller/productController.js'
 
 const productRouter = express.Router()
 
 productRouter.post('/create-product', upload.array("images"), createProduct)
-
+productRouter.get('/get-all-products-shop/:id', getAllProductsShop)
 export default productRouter
