ef7cc3f3d80c1c3cde608f27806bfdac3e55c0ed
Amarta113 <amartawaghani@gmail.com>
2026-07-01 22:27:28 +0500

    fixed active params value

--- frontend/src/pages/Shop/ShopAllEvents.jsx
+++ frontend/src/pages/Shop/ShopAllEvents.jsx
@@ -9,7 +9,7 @@ function ShopAllEvents() {
       <DashboardHeader />
       <div className='flex justify-between w-full'>
         <div className='w-[80px] md:w-[330px]'>
-          <DashboardSideBar active={3} />
+          <DashboardSideBar active={5} />
         </div>
         <div className='w-full justify-center flex'>
           <ShopAllEvents />
