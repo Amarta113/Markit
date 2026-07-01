ddc72dcf318beb28b3d2df1180f47adb9ae96621
Amarta113 <amartawaghani@gmail.com>
2026-07-01 18:08:45 +0500

    Added event components imports

--- frontend/src/routes/ShopRoutes.jsx
+++ frontend/src/routes/ShopRoutes.jsx
@@ -1,9 +1,13 @@
 import ShopHomePage from '../pages/Shop/ShopHomePage'
 import ShopDashboardPage from '../pages/Shop/ShopDashboardPage'
 import ShopAllProducts from '../pages/Shop/ShopAllProducts.jsx'
+import ShopCreateEvents from '../pages/Shop/ShopCreateEvents.jsx'
+import ShopAllEvents from '../pages/Shop/ShopAllEvents.jsx'
 
 export {
     ShopHomePage,
     ShopDashboardPage,
-    ShopAllProducts
+    ShopAllProducts,
+    ShopCreateEvents,
+    ShopAllEvents
 }
