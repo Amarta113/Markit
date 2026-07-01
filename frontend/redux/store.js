595594ee3a89aba63fee0b4530fbd9259a95cfbb
Amarta113 <amartawaghani@gmail.com>
2026-07-01 18:08:30 +0500

    Added event reducer in store

--- frontend/redux/store.js
+++ frontend/redux/store.js
@@ -4,6 +4,7 @@ import { cartReducer } from './reducer/cartReducer.js';
 import { wishlistReducer } from './reducer/wishlistReducer.js';
 import { sellerReducer } from './reducer/sellerReducer.js';
 import { productReducer } from './reducer/productReducer.js';
+import { eventReducer } from './reducer/eventReducer.js';
 
 
 const store = configureStore({
@@ -12,7 +13,8 @@ const store = configureStore({
     cart: cartReducer,
     seller: sellerReducer,
     wishlist: wishlistReducer,
-    products: productReducer
+    products: productReducer,
+    events: eventReducer
   },
 }); 
 
