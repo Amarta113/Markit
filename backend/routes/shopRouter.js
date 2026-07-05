baf8a4adc716a091dd9ff090fbce0634efdcd852
Amarta113 <amartawaghani@gmail.com>
2026-07-05 19:41:08 +0500

    Add getShopInfo route to shopRouter

--- backend/routes/shopRouter.js
+++ backend/routes/shopRouter.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createShop, activateSeller, loginSeller, loadSeller, logoutSeller} from '../controller/shopController.js';
+import { createShop, activateSeller, loginSeller, loadSeller, logoutSeller, getShopInfo} from '../controller/shopController.js';
 import upload from '../multer.js';
 import { catchAsyncError } from '../middleware/catchAsyncError.js';
 import { isSeller } from '../middleware/auth.js';
@@ -10,4 +10,5 @@ shopRouter.post("/activation", activateSeller)
 shopRouter.post("/login-seller", loginSeller);
 shopRouter.get("/get-seller", isSeller, loadSeller)
 shopRouter.get("/logout-seller", isSeller, logoutSeller)
+shopRouter.get('/get-shop-info/:id', getShopInfo)
 export default shopRouter;
