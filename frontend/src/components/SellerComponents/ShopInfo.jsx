a4d036434607635540f4f44f758b9ac96e8bb9a6
Amarta113 <amartawaghani@gmail.com>
2026-07-05 22:19:55 +0500

    Refactor ShopInfo component to fetch shop data from API and update state management

--- frontend/src/components/SellerComponents/ShopInfo.jsx
+++ frontend/src/components/SellerComponents/ShopInfo.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { server } from '../../server'
+import { useParams } from 'react-router-dom'
+import { getAllProductsShop } from '../../../redux/actions/productActions'
+import axios from 'axios'
 
 const ShopInfo = ({ isOwner }) => {
-    const { seller } = useSelector((state) => state.seller)
+    const { products } = useSelector(state => state.products)
+    const { id } = useParams()
+    const [data, setData] = useState({})
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true)
+        axios.get(`${server}/shop/get-shop-info/${id}`)
+        .then((res) => {
+            setData(res.data.shop)
+            setIsLoading(false)
+        })
+        .catch((error) => {
+            console.log(error)
+            setIsLoading(false)
+        })
+    }, [])
+
     async function logoutHandler () {
         try {
             setIsLoading(true)
-            const res = await axios.get(
-                `${server}/seller/seller-logout`,
+            const res = await axios.get( `${server}/seller/seller-logout`,
                 {
                     withCredential: true
-                }
-            )
+                })
             if (res.data.success) {
                 window.location.href = '/shop-create'
             } else {
@@ -31,19 +49,19 @@ const ShopInfo = ({ isOwner }) => {
                         className='w-[150px] h-[150px] object-cover rounded-full' />
                 </div>
                 <h3 className='text-center py-2 text-[20px]'>
-                    {seller.name}
+                    {data.name}
                 </h3>
                 <p className='text-[16px] text-[#000000a6] p-[10px] flex item-center'>
-                    {seller.description}
+                    {data.description}
                 </p>
             </div>
             <div className="p-3">
                 <h5 className='font-[600]' >Address</h5>
-                <h4 className='text-[#000000a6]'>{seller.address}</h4>
+                <h4 className='text-[#000000a6]'>{data.address}</h4>
             </div>
             <div className="p-3">
                 <h5 className='font-[600]' >Phone Number</h5>
-                <h4 className='text-[#000000a6]'>{seller.phoneNumber}</h4>
+                <h4 className='text-[#000000a6]'>{data.phoneNumber}</h4>
             </div>
             <div className="p-3">
                 <h5 className='font-[600]'>Total Products</h5>
@@ -56,7 +74,7 @@ const ShopInfo = ({ isOwner }) => {
             <div className="p-3">
                 <h5 className='font-[600]' >Joined On</h5>
                 <h4 className='text-[#000000a6]'>
-                    {seller.createdAt.slice(0, 10)}
+                    {data?.createdAt.slice(0, 10)}
                 </h4>
             </div>
             {isOwner && (
