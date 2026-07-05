9932e99ce4fc4bc795e99d88a3babed56fca19a9
Amarta113 <amartawaghani@gmail.com>
2026-07-05 19:41:43 +0500

    Added getShopInfo  func to shopController for retrieving shop details

--- backend/controller/shopController.js
+++ backend/controller/shopController.js
@@ -196,3 +196,16 @@ export const logoutSeller = catchAsyncError(async(req, res, next) => {
         return next(new ErrorHandler(error.message, 500))
     }
 }) 
+
+// get shop information
+export const getShopInfo = catchAsyncError(async(req, res, next) => {
+    try {
+        const shop = await Shop.findById(req.params.id)
+        res.status(201).json({
+            success: true,
+            shop
+        })
+    } catch(error){
+        return next(new ErrorHandler(error.message, 500))
+    }
+})
