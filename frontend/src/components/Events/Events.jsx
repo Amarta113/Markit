d4bc254082189d6b2f4cae8c8a2ee9a8c80baeab
Amarta113 <amartawaghani@gmail.com>
2026-07-06 17:06:29 +0500

    Refactor Events component to utilize Redux for event data and improve loading state handling

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import styles from '../../styles/styles';
 import EventCard from './EventCard'
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react';
 
 const Events = () => {
+    const { allEvents, isLoading } = useSelector((state) => state.events)
     return (
         <div>
-            <div className={`${styles.section}`}>
-                <div className={`${styles.heading}`}>
-                    <h1 className='font-[800] text-[2rem] font-[Roboto]'>Popular Events</h1>
-                </div>
-                <div className="w-full grid mb-14">
-                    <EventCard/>
-                </div>
-            </div>
+            {
+                !isLoading && (
+                    <div className={`${styles.section}`}>
+                        <div className={`${styles.heading}`}>
+                            <h1 className='font-[800] text-[2rem] font-[Roboto]'>Popular Events</h1>
+                        </div>
+                        <div className="w-full grid mb-14">
+                            <EventCard data={allEvents && allEvents[0]} />
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
