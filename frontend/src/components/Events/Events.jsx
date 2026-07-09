bab005b8a6701533f7f82a00eb4b1fbf3462fa85
Amarta113 <amartawaghani@gmail.com>
2026-07-09 22:13:28 +0500

    Fix Events component to ensure EventCard renders only when allEvents is available

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -2,20 +2,19 @@ import React from 'react'
 import styles from '../../styles/styles';
 import EventCard from './EventCard'
 import { useSelector } from 'react-redux'
-import { useEffect } from 'react';
 
 const Events = () => {
     const { allEvents, isLoading } = useSelector((state) => state.events)
     return (
         <div>
             {
-                !isLoading && (
+                !isLoading && allEvents?.length > 0 && (
                     <div className={`${styles.section}`}>
                         <div className={`${styles.heading}`}>
                             <h1 className='font-[800] text-[2rem] font-[Roboto]'>Popular Events</h1>
                         </div>
                         <div className="w-full grid mb-14">
-                            <EventCard data={allEvents && allEvents[0]} />
+                            <EventCard data={allEvents[0]} />
                         </div>
                     </div>
                 )
