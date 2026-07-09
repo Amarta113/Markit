1b683c98ceed9e5252e139c14ea08f232924851b
Amarta113 <amartawaghani@gmail.com>
2026-07-09 21:33:56 +0500

    Add missing deleteProduct and getAllProductsShop actions to productReducer

--- frontend/redux/reducer/productReducer.js
+++ frontend/redux/reducer/productReducer.js
@@ -1,5 +1,5 @@
 import { createReducer } from "@reduxjs/toolkit";
-import { createProduct, getAllProducts } from "../actions/productActions";
+import { createProduct, deleteProduct, getAllProducts, getAllProductsShop } from "../actions/productActions";
 
 const initialState = {
     isLoading: true
