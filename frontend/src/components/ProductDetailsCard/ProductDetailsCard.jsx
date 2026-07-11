44367b642a06a47525abfef58f2f09b63582e745
Amarta113 <amartawaghani@gmail.com>
2026-07-11 13:44:13 +0500

    Refactor ProductDetailsCard to integrate Redux for cart management and enhance add to cart functionality

--- frontend/src/components/ProductDetailsCard/ProductDetailsCard.jsx
+++ frontend/src/components/ProductDetailsCard/ProductDetailsCard.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
-import {RxCross1} from "react-icons/rx"
+import { RxCross1 } from "react-icons/rx"
+import useSelector, { useDispatch } from 'react-redux'
 import styles from '../../styles/styles'
 import { AiOutlineHeart, AiOutlineMessage, AiOutlineShoppingCart } from 'react-icons/ai'
+import { taost } from 'react-toastify'
+import { addToCart } from '../../../redux/actions/cartActions'
 
-const ProductDetailsCard = ({setOpen, data}) => {
+const ProductDetailsCard = ({ setOpen, data }) => {
+    const { cart } = useSelector((state) => state.cart)
+    const dispatch = useDispatch()
     const [count, setCount] = useState(1)
     const [click, setClick] = useState(false)
     const [select, setSelect] = useState(false)
@@ -12,12 +17,27 @@ const ProductDetailsCard = ({setOpen, data}) => {
         return null
     }
     const decrementCount = () => {
-        if(count>1){
-            setCount(count-1)
+        if (count > 1) {
+            setCount(count - 1)
         }
     }
     const incrementCount = () => {
-            setCount(count+1)
+        setCount(count + 1)
+    }
+
+    const addToCartHandler = (id) => {
+        const isItemExist = cart && cart.find((i) => i._id === id)
+        if (!isItemExist) {
+            toast.error("Item already in cart!")
+        } else {
+            if (data.stock < count) {
+                return toast.error("Product stock is limited")
+            } else {
+                const cartData = { ...data, qty: count }
+                dispatch(addToCart(cartData))
+                toast.success("Item added to cart successfully.")
+            }
+        }
     }
     const removeFromWishListhandler = () => {
         return null
@@ -29,33 +49,33 @@ const ProductDetailsCard = ({setOpen, data}) => {
 
     return (
         <div className='bg-[#fff]'>
-            {data? (
+            {data ? (
                 <div className='fixed w-full h-screen top-0 left-0 bg-[#00000030] z-40 flex items-center justify-center'>
                     <div className="w-[90%] md:w-[70%] h-[90vh] overflow-y-scroll 800px:h-[75vh] bg-white rounded-md shadow-sm relative p-4">
                         <RxCross1 size={30} className="absolute right-3 top-3 z-50"
-                        onClick={() => setOpen(false)}
+                            onClick={() => setOpen(false)}
                         />
                         <div className="block w-full md:flex">
                             <div className="w-full md:w-[50%]">
                                 <img src={data.image_Url[0].url} alt="" />
                                 <div className='flex'>
                                     <img src={data.shop.shop_avatar.url} alt=""
-                                    className='w-[50px] h-[50px] rounded-full mr-2'
+                                        className='w-[50px] h-[50px] rounded-full mr-2'
                                     />
                                     <div>
-                                    <h3 className={styles.shop_name}>
-                                        {data.shop.name}
-                                    </h3>
-                                    <h5 className='pb-3 text-[15px]'>
-                                        ({data.shop.ratings})Ratings
-                                    </h5>
-                                    </div>  
+                                        <h3 className={styles.shop_name}>
+                                            {data.shop.name}
+                                        </h3>
+                                        <h5 className='pb-3 text-[15px]'>
+                                            ({data.shop.ratings})Ratings
+                                        </h5>
+                                    </div>
                                 </div>
                                 <div className={`${styles.button} bg-[#000] mt-4 rounded-[4px] h-11`}
                                     onClick={handleMessageSubmit}>
-                                        <span className="text-[#fff] flex items-center">
-                                          Send Message <AiOutlineMessage className='ml-1'/>
-                                        </span>
+                                    <span className="text-[#fff] flex items-center">
+                                        Send Message <AiOutlineMessage className='ml-1' />
+                                    </span>
                                 </div>
                                 <h5 className='text-[16px] text-[red] mt-5'>
                                     ({data.total_sell}) Sold out
@@ -70,51 +90,52 @@ const ProductDetailsCard = ({setOpen, data}) => {
                                     <h4 className={`${styles.productDiscountPrice}`}>
                                         {data.discount_price}$
                                     </h4>
-                                    <h3 className={`${styles.price}`}>{data.price? data.price + "$": null }</h3>
-                                </div>    
-                            <div className="flex items-center mt-12 justify-between pr-3">
-                                <div>
-                                    <button 
-                                    className='bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out'
-                                    onClick={decrementCount}>
-                                    -
-                                    </button>
-                                    <span className="bg-gray-200 text-gray-800 font-medium px-4 py-[8px]">{count}</span>
-                                    <button className='bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out'
-                                    onClick={incrementCount}>
-                                    +
-                                    </button>
+                                    <h3 className={`${styles.price}`}>{data.price ? data.price + "$" : null}</h3>
                                 </div>
-                                <div>
-                                {click? (
-                                    <AiFillHeart
-                                        size={30}
-                                        className="cursor-pointer"
-                                        onClick={() => removeFromWishListhandler(data)}
-                                        color={click? "red" : "#333"}
-                                        title="Remove from wishlist"
-                                    />
-                                    ) : (
-                                    <AiOutlineHeart
-                                        size={30}
-                                        className="cursor-pointer"
-                                        onClick={() => addToWishListhandler(data)}
-                                        color={click? "red" : "#333"}
-                                        title="Add to wishlist"
-                                    />
-                                    )}  
+                                <div className="flex items-center mt-12 justify-between pr-3">
+                                    <div>
+                                        <button
+                                            className='bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out'
+                                            onClick={decrementCount}>
+                                            -
+                                        </button>
+                                        <span className="bg-gray-200 text-gray-800 font-medium px-4 py-[8px]">{count}</span>
+                                        <button className='bg-gradient-to-r from-teal-400 to-teal-500 text-white font-bold rounded-l px-4 py-2 shadow-lg hover:opacity-75 transition duration-300 ease-in-out'
+                                            onClick={incrementCount}>
+                                            +
+                                        </button>
+                                    </div>
+                                    <div>
+                                        {click ? (
+                                            <AiFillHeart
+                                                size={30}
+                                                className="cursor-pointer"
+                                                onClick={() => removeFromWishListhandler(data)}
+                                                color={click ? "red" : "#333"}
+                                                title="Remove from wishlist"
+                                            />
+                                        ) : (
+                                            <AiOutlineHeart
+                                                size={30}
+                                                className="cursor-pointer"
+                                                onClick={() => addToWishListhandler(data)}
+                                                color={click ? "red" : "#333"}
+                                                title="Add to wishlist"
+                                            />
+                                        )}
+                                    </div>
+                                </div>
+                                <div className={`${styles.button} mt-6 rounded-[4px] h-11 flex items-center`}
+                                    onClick={() => addToCartHandler(data._id)}>
+                                    <span className='text-[#fff] flex items-center'>
+                                        Add to cart <AiOutlineShoppingCart className='ml-1' />
+                                    </span>
                                 </div>
                             </div>
-                            <div className={`${styles.button} mt-6 rounded-[4px] h-11 flex items-center`}>
-                                <span className='text-[#fff] flex items-center'>
-                                    Add to cart <AiOutlineShoppingCart className='ml-1'/>
-                                </span>
-                            </div>
-                        </div>
                         </div>
                     </div>
                 </div>
-                ) : null}
+            ) : null}
         </div>
     )
 }
