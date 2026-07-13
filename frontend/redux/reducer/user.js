49b09a4969d65a47b826f257fa1256645b008c55
Amarta113 <amartawaghani@gmail.com>
2026-07-13 22:14:29 +0500

    Implement user update and address management actions in user reducer

--- frontend/redux/reducer/user.js
+++ frontend/redux/reducer/user.js
@@ -25,6 +25,40 @@ export const userReducer = createReducer(initialState, (builder) => {
       state.isAuthenticated = false
       state.user = null
     })
+    // update user data
+    .addCase(updateUser.pending, state => {
+      state.loading = true;
+    })
+    .addCase(updateUser.fulfilled, (state, action) => {
+      state.loading = false;
+      state.user = action.payload
+    })
+    .addCase(updateUser.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload
+    })
+    .addCase(updateAddresses.pending, state => {
+      state.addressloading = true;
+    })
+    .addCase(updateAddresses.fulfilled, (state, action) => {
+      state.addressloading = false;
+      state.user = action.payload
+    })
+    .addCase(updateAddresses.rejected, (state, action) => {
+      state.addressloading = false;
+      state.error = action.payload
+    })
+    .addCase(deleteAddress.pending, state => {
+      state.deletingAddress = true;
+    })
+    .addCase(deleteAddress.fulfilled, (state, action) => {
+      state.deletingAddress = false;
+      state.user = action.payload
+    })
+    .addCase(deleteAddress.rejected, (state, action) => {
+      state.deletingAddress = false;
+      state.error = action.payload
+    })
     .addCase(clearErrors, (state) => {
       state.error = null
     })
