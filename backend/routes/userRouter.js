2458bbcb3ee46e426227f9951aa99d743e118455
Amarta113 <amartawaghani@gmail.com>
2026-07-14 22:20:20 +0500

    added user-related routes for updating user info, avatar, addresses, and password

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { register, activateUser, loginUser, loadUser, logoutUser} from '../controller/userController.js';
+import { register, activateUser, loginUser, loadUser, logoutUser, updateAddresses, updateUserPassword, updateUser, deleteUserAddress, updateUserAvatar} from '../controller/userController.js';
 import upload from '../multer.js';
 import { catchAsyncError } from '../middleware/catchAsyncError.js';
 import { isAuthenticated } from '../middleware/auth.js';
@@ -10,4 +10,9 @@ router.post("/activation", activateUser)
 router.post("/login-user", loginUser);
 router.get("/get-user", isAuthenticated, loadUser)
 router.get("/logout-user", isAuthenticated, logoutUser)
+router.put("/update-user-info", isAuthenticated, updateUser)
+router.put("/update-user-avatar", isAuthenticated, upload.single("image"), updateUserAvatar)
+router.put("/update-user-addresses", isAuthenticated, updateAddresses)
+router.put('/update-user-password', isAuthenticated, updateUserPassword)
+router.delete('/delete-user-address/:id', isAuthenticated, deleteUserAddress)
 export default router;
