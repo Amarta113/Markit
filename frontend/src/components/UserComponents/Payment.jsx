844b4b8e030a3aad0f40a67afcf79a967ed092f9
Amarta113 <amartawaghani@gmail.com>
2026-07-26 22:00:12 +0500

    feat: implement order data retrieval and integrate CartData component in Payment

--- frontend/src/components/UserComponents/Payment.jsx
+++ frontend/src/components/UserComponents/Payment.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import CartData from '../../components/UserComponents/CartData'
 
 const Payment = () => {
+    const [orderData, setOrderData] = useState([])
+    useEffect(() => {
+    const orderData = JSON.parse(localStorage.getItem("latestOrder"))
+    setOrderData(orderData)
+    }, [])
     return (
-        <div>
-            <h1>
-                Default payment page
-            </h1>
+        <div className='w-full flex flex-col items-center py-8'>
+            <div className="w-[90%] 1000px:w-[70%] block md:flex">
+                <div className="w-full md:w-[65%]">
+                    <PaymentInfo />
+                </div>
+                <div className="w-full md:w-[35%] md:mt-0 mt-8">
+                    <CartData orderData={orderData}/>
+                </div>
+            </div>
         </div>
     )
 }
