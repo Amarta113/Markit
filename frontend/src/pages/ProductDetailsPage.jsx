c9844bdbdc3da898c2e9f007f887486d1467c7e7
Amarta113 <amartawaghani@gmail.com>
2026-07-29 21:37:54 +0500

    feat: enhance ProductDetailsPage to support event data retrieval and conditional rendering

--- frontend/src/pages/ProductDetailsPage.jsx
+++ frontend/src/pages/ProductDetailsPage.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
 import Footer from "../components/Layout/Footer"
 import Header from '../components/Layout/Header'
 import ProductDetails from "../components/UserComponents/ProductDetails";
@@ -7,20 +7,31 @@ import SuggestedProduct from '../components/UserComponents/SuggestedProduct.jsx'
 import { useSelector } from 'react-redux';
 
 const ProductDetailsPage = () => {
-    const {allProducts} = useSelector(state => state.products)
-    const {id} = useParams()
+    const { allProducts } = useSelector(state => state.products)
+    const { allEvents } = useSelector((state) => state.events)
+    const { id } = useParams()
     const [data, setData] = useState(null)
+    const [searchParams] = useSearchParams()
+    const eventData = searchParams.get("isEvent")
 
     useEffect(() => {
-        const data = allProducts && allProducts.find((i) => i._id === id)
+        if(eventData !== null){
+            const data = allEvents && allEvents.find((i) => i._id === id)
+        }else{
+            const data = allProducts && allProducts.find((i) => i._id === id)
+        }
         setData(data)
-    }, [data, allProducts])
+    }, [allProducts, allEvents])
     return (
         <div>
             <Header />
             <ProductDetails data={data} />
             {
-             data && <SuggestedProduct data={data} />   
+                !eventData && (
+                <>
+                data && <SuggestedProduct data={data} />
+                </>
+            )
             }
             <Footer />
         </div>
