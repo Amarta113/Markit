b3683780f43fd4dac6ae0648ae8a2424d2de73c4
Amarta113 <amartawaghani@gmail.com>
2026-07-31 22:24:24 +0500

    feat: add route to get all orders for a specific seller by shop ID

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import {isAuthenticated, isSeller} from '../middleware/auth.js'
-import { createOrder, getAllOrders } from '../controller/orderController.js'
+import { createOrder, getAllOrders, getAllOrdersShop } from '../controller/orderController.js'
 
 const orderRouter = express.Router()
 
 orderRouter.post('/create-order', createOrder)
 orderRouter.get('/geta-all-orders/:userId', getAllOrders)
+orderRouter.get('/get-seller-all-orders/:shopId', getAllOrdersShop)
 
 
 export default orderRouter
