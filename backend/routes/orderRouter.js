84bd12f0b9bfb294a42739c47e3f28f171fee5d6
Amarta113 <amartawaghani@gmail.com>
2026-08-02 22:11:41 +0500

    feat: add update order status route to orderRouter

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import {isAuthenticated, isSeller} from '../middleware/auth.js'
-import { createOrder, getAllOrders, getAllOrdersShop } from '../controller/orderController.js'
+import { createOrder, getAllOrders, getAllOrdersShop, updateOrderStatus } from '../controller/orderController.js'
 
 const orderRouter = express.Router()
 
 orderRouter.post('/create-order', createOrder)
 orderRouter.get('/geta-all-orders/:userId', getAllOrders)
 orderRouter.get('/get-seller-all-orders/:shopId', getAllOrdersShop)
+orderRouter.put('/update-order-status/:id', isSeller, updateOrderStatus)
 
 
 export default orderRouter
