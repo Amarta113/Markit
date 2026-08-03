df863a14dd6fdee9197d5809c2c1d9690443d31e
Amarta113 <amartawaghani@gmail.com>
2026-08-03 19:12:07 +0500

    fix: add createReviewForProduct route to productRouter

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import upload from '../multer.js'
 import {isAuthenticated, isSeller} from '../middleware/auth.js'
-import { createProduct, getAllProductsShop, deleteProduct } from '../controller/productController.js'
+import { createProduct, getAllProductsShop, deleteProduct, createReviewForProduct } from '../controller/productController.js'
 
 const productRouter = express.Router()
 
 productRouter.post('/create-product', upload.array("images"), createProduct)
 productRouter.get('/get-all-products-shop/:id', getAllProductsShop)
 productRouter.delete('/delete-shop-product/:id', isSeller, deleteProduct)
+productRouter.put('/create-new-review', isAuthenticated, createReviewForProduct)
 export default productRouter
