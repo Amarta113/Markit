6c6713d8bb6b775b00d92a2a4eb6d3302409b28e
Amarta113 <amartawaghani@gmail.com>
2026-08-04 22:22:21 +0500

    feat: add orderRefund route to orderRouter for processing refunds

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {isAuthenticated, isSeller} from '../middleware/auth.js'
-import { createOrder, getAllOrders, getAllOrdersShop, updateOrderStatus } from '../controller/orderController.js'
+import { createOrder, getAllOrders, getAllOrdersShop, updateOrderStatus, orderRefund } from '../controller/orderController.js'
 
 const orderRouter = express.Router()
 
@@ -8,6 +8,7 @@ orderRouter.post('/create-order', createOrder)
 orderRouter.get('/geta-all-orders/:userId', getAllOrders)
 orderRouter.get('/get-seller-all-orders/:shopId', getAllOrdersShop)
 orderRouter.put('/update-order-status/:id', isSeller, updateOrderStatus)
+orderRouter.put('/order-refund/:id', orderRefund)
 
 
 export default orderRouter
