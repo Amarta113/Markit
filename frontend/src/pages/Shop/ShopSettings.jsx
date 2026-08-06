5f16d660794bd9ca66eb03d3721f846e5ad73712
Amarta113 <amartawaghani@gmail.com>
2026-08-06 22:10:14 +0500

    feat: create ShopSettingsPage component

---
+++ frontend/src/pages/Shop/ShopSettings.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ShopSettings from '../../components/ShopSettings.jsx'
+
+const ShopSettingsPage = () => {
+  return (
+    <div>
+      <Header />
+      <ShopSettings />
+      <Footer />
+    </div>
+  )
+}
+
+export default ShopSettingsPage
