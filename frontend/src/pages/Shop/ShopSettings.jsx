8ba2265a18e05c3b718cb1d372f51de67cb11144
Amarta113 <amartawaghani@gmail.com>
2026-08-07 22:22:30 +0500

    feat: restructure ShopSettingsPage layout with DashboardHeader and DashboardSidebar components

--- frontend/src/pages/Shop/ShopSettings.jsx
+++ frontend/src/pages/Shop/ShopSettings.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ShopSettings from '../../components/ShopSettings.jsx'
+import DashboardHeader from "../../components/SellerComponents/DashboardHeader.jsx"
+import DashboardSidebar from '../../components/SellerComponents/DashboardSidebar.jsx'
 
 const ShopSettingsPage = () => {
   return (
     <div>
-      <Header />
-      <ShopSettings />
-      <Footer />
+      <DashboardHeader/>
+      <div className="flex items-start justify-between w-full">
+        <div className="w-[80%] md:w-[330px]">
+          <DashboardSidebar active={11}/>
+        </div>
+        <ShopSettings />
+      </div>
     </div>
   )
 }
