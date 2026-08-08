9689e2f0e8628257a06be938a97bfd5b81cbef9f
Amarta113 <amartawaghani@gmail.com>
2026-08-08 22:23:25 +0500

    fix: correct import and refactor createNewConversation function for consistency

--- backend/controller/conversationController.js
+++ backend/controller/conversationController.js
@@ -1,26 +1,31 @@
 import { catchAsyncError } from "../middleware/catchAsyncError";
 import ErrorHandler from "../middleware/error";
-import Messages from "../models/conversation";
+import Conversation from "../models/conversation.js";
 
-export const createNewConversation = catchAsyncError(async(req, res, next) => {
-        try{
-            const {groupTitle, userId, sellerId} = req.body
-            const isConversationExist = await Messages.findOn({groupTitle})
+export const createNewConversation = catchAsyncError(async (req, res, next) => {
+    try {
+        const { groupTitle, userId, sellerId } = req.body
+        const isConversationExist = await Conversation.findOn({ groupTitle })
 
-            if(!isConversationExist){
-                return next(new ErrorHandler("Conversation group already exists with this seller"), 500)
-            }
-
-            const conversation = await Messages.create({
-                members: [userId, sellerId],
-                groupTitle: groupTitle
-            })
-
+        if (isConversationExist) {
+            const conversation = isConversationExist
             res.status(201).json({
                 success: true,
                 conversation
             })
-        }catch(error){
-            return next(new ErrorHandler(error.response.message, 500))
+        } else {
+            const conversation = await Conversation.create({
+                members: [userId, sellerId],
+                groupTitle: groupTitle
+            })
+            res.status(201).json({
+                success: true,
+                conversation
+            })
         }
+    } catch (error) {
+        return next(new ErrorHandler(error.response.message, 500))
+    }
 })
+
+
