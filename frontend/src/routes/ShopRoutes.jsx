c4f96b982e88c6c1bc7c00023aa24d58405e4802
Amarta113 <amartawaghani@gmail.com>
2026-08-08 20:45:19 +0500

    fix: update import for ShopSettingsPage and add ShopInboxPage to exports

--- frontend/src/routes/ShopRoutes.jsx
+++ frontend/src/routes/ShopRoutes.jsx
@@ -7,8 +7,9 @@ import ShopAllEvents from '../pages/Shop/ShopAllEvents.jsx'
 import ShopAllCoupons from '../pages/Shop/ShopAllCoupons.jsx'
 import ShopOrderDetails from '../pages/Shop/ShopOrderDetails.jsx'
 import ShopAllRefunds from "../pages/Shop/ShopAllRefunds.jsx"
-import ShopSettingsPage from '../pages/Shop/ShopSettingsPage.jsx'
+import ShopSettingsPage from '../pages/Shop/ShopSettings'
 import ShopWithDrawMoneyPage from '../pages/Shop/ShopWithDrawMoneyPage.jsx'
+import ShopInboxPage from '../pages/Shop/ShopInboxPage.jsx'
 export {
     ShopHomePage,
     ShopDashboardPage,
@@ -20,5 +21,6 @@ export {
     ShopOrderDetails,
     ShopAllRefunds,
     ShopSettingsPage,
-    ShopWithDrawMoneyPage
+    ShopWithDrawMoneyPage,
+    ShopInboxPage
 }
