ec726ede201ae73eb416a3d6c239b66ec1fe81b2
Amarta113 <amartawaghani@gmail.com>
2026-08-09 21:13:31 +0500

    added import path

--- backend/routes/messageRouter.js
+++ backend/routes/messageRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import upload from "../multer";
+import {createNewMessage} from '../controller/messageController'
 
 const messageRouter = express.Router()
 messageRouter.post('/create-new-message', upload.array('images'), createNewMessage)
