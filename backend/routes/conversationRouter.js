363ba85c9e95e11c958e9167326c7f2401573fa9
Amarta113 <amartawaghani@gmail.com>
2026-08-09 15:08:08 +0500

    feat: add route to retrieve all conversations for a seller

--- backend/routes/conversationRouter.js
+++ backend/routes/conversationRouter.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import {isAuthenticated, isSeller} from '../middleware/auth.js'
-import { createNewConversation } from '../controller/conversationController.js'
+import { createNewConversation,getAllConversationSeller } from '../controller/conversationController.js'
 
 const conversationRouter = express.Router()
 
 conversationRouter.post('/create-new-conversation', createNewConversation)
+conversationRouter.get('/get-all-conversation-seller/:id', isSeller, getAllConversationSeller)
 export default conversationRouter
