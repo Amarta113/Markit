de36021366d38b436ad49157afd18af6c1c85a39
Amarta113 <amartawaghani@gmail.com>
2026-08-10 22:33:02 +0500

    feat: add route to retrieve user information by ID

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { register, activateUser, loginUser, loadUser, logoutUser, updateAddresses, updateUserPassword, updateUser, deleteUserAddress, updateUserAvatar} from '../controller/userController.js';
+import { register, activateUser, loginUser, loadUser, logoutUser, updateAddresses, updateUserPassword, updateUser, deleteUserAddress, updateUserAvatar, getUserInfo} from '../controller/userController.js';
 import upload from '../multer.js';
 import { catchAsyncError } from '../middleware/catchAsyncError.js';
 import { isAuthenticated } from '../middleware/auth.js';
@@ -15,4 +15,5 @@ router.put("/update-user-avatar", isAuthenticated, upload.single("image"), updat
 router.put("/update-user-addresses", isAuthenticated, updateAddresses)
 router.put('/update-user-password', isAuthenticated, updateUserPassword)
 router.delete('/delete-user-address/:id', isAuthenticated, deleteUserAddress)
+router.get('/user-info/:id', getUserInfo)
 export default router;
