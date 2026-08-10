3369491d91c17343cfb24109497eb18afb711623
Amarta113 <amartawaghani@gmail.com>
2026-08-10 22:32:52 +0500

    feat: add route to retrieve all products

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import upload from '../multer.js'
 import {isAuthenticated, isSeller} from '../middleware/auth.js'
-import { createProduct, getAllProductsShop, deleteProduct, createReviewForProduct } from '../controller/productController.js'
+import { createProduct, getAllProductsShop, deleteProduct, createReviewForProduct, getAllProducts } from '../controller/productController.js'
 
 const productRouter = express.Router()
 
 productRouter.post('/create-product', upload.array("images"), createProduct)
 productRouter.get('/get-all-products-shop/:id', getAllProductsShop)
+productRouter.get('/get-all-products', getAllProducts)
 productRouter.delete('/delete-shop-product/:id', isSeller, deleteProduct)
 productRouter.put('/create-new-review', isAuthenticated, createReviewForProduct)
 export default productRouter
