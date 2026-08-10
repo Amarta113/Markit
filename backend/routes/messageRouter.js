a7fafc50161e705e00c9c55225d272c1ae7a086c
Amarta113 <amartawaghani@gmail.com>
2026-08-10 22:32:43 +0500

    feat: add route to retrieve all messages by user ID

--- backend/routes/messageRouter.js
+++ backend/routes/messageRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import upload from "../multer";
-import {createNewMessage} from '../controller/messageController'
+import {createNewMessage, getAllMessages} from '../controller/messageController'
 
 const messageRouter = express.Router()
 messageRouter.post('/create-new-message', upload.array('images'), createNewMessage)
+messageRouter.get('/get-all-messages/:id', getAllMessages)
 
 export default messageRouter
