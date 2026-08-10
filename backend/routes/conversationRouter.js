a11ef0ded26cb64aadd9bdf31222fb3f904e9599
Amarta113 <amartawaghani@gmail.com>
2026-08-10 22:32:15 +0500

    feat: add routes for getting user conversations and updating last message

--- backend/routes/conversationRouter.js
+++ backend/routes/conversationRouter.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import {isAuthenticated, isSeller} from '../middleware/auth.js'
-import { createNewConversation,getAllConversationSeller } from '../controller/conversationController.js'
+import { createNewConversation, getAllConversationSeller, updateLastMessage, getAllConversationUser } from '../controller/conversationController.js'
 
 const conversationRouter = express.Router()
 
 conversationRouter.post('/create-new-conversation', createNewConversation)
 conversationRouter.get('/get-all-conversation-seller/:id', isSeller, getAllConversationSeller)
+conversationRouter.get('/get-all-conversation-user/:id', getAllConversationUser)
+conversationRouter.put('/update-last-message/:id', updateLastMessage)
 export default conversationRouter
