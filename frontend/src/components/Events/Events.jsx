4dabe2c5aa44c71d6deeb8c9e0db52e2c1ae53cf
Amarta113 <amartawaghani@gmail.com>
2026-08-12 20:44:37 +0500

    fix: correct import statement and simplify event mapping in Events component

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import styles from '../../styles/styles';
+import styles from '../../styles/styles.js';
 import EventCard from './EventCard'
+import Loader from '../UserComponents/Loader.jsx'
 import { useSelector } from 'react-redux'
 
 const Events = () => {
     const { allEvents, isLoading } = useSelector((state) => state.events)
     return (
         <div>
-            {
-                !isLoading && allEvents?.length > 0 && (
+            {isLoading ? (
+                <Loader />    
+            ) : (
                     <div className={`${styles.section}`}>
                         <div className={`${styles.heading}`}>
                             <h1 className='font-[800] text-[2rem] font-[Roboto]'>Popular Events</h1>
@@ -16,16 +18,14 @@ const Events = () => {
                         <div className="w-full grid mb-14">
                             {
                                 allEvents && allEvents?.length !== 0 &&
-                                allEvents?.map((event, i) => {
-                                    (
-                                        <EventCard 
-                                        data={event}
-                                        key={i}
-                                        active={true}
-                                        isLoading={isLoading}
-                                        />
-                                    )
-                                })
+                                allEvents?.map((event, i) => (
+                                    <EventCard 
+                                    data={event}
+                                    key={i}
+                                    active={true}
+                                    isLoading={isLoading}
+                                    />
+                                ))
                             }
                             {allEvents?.length === 0 && <h4>No Product Events Running!</h4>}
                         </div>
