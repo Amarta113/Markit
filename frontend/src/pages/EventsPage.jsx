6f2c29b70930b3c7aa83cc4a5d921087e2763796
Amarta113 <amartawaghani@gmail.com>
2026-08-12 16:32:29 +0500

    fix: update EventsPage to handle loading state and display multiple events

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -3,16 +3,21 @@ import { useSelector } from 'react-redux'
 import Header from '../components/Layout/Header'
 import Footer from '../components/Layout/Footer'
 import EventCard from '../components/Events/EventCard'
-import styles from '../styles/styles'
+import styles from '../styles/styles.js'
 
 export default function EventsPage() {
-    const { allEvents } = useSelector((state) => state.events)
+    const { allEvents, isLoading } = useSelector((state) => state.events)
 
     return (
         <div>
             <Header activeHeading={4} />
             <main className={`${styles.section} max-w-6xl py-10 pb-16 px-4 sm:px-0`}>
-            <EventCard active={true} data={allEvents?.[0]} />
+                {allEvents?.length !== 0 &&
+                    allEvents &&
+                    allEvents.map((event, i) => (
+                        <EventCard data={event} key={i} active={true} isLoading={isLoading} />
+                    ))}
+                {allEvents?.length === 0 && <h4>No Product Events Running!</h4>}
             </main>
             <Footer />
         </div>
