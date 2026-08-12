4e77ea5a671474e21e66b795719adba7b3c86bc3
Amarta113 <amartawaghani@gmail.com>
2026-08-12 16:32:44 +0500

    fix: improve layout and structure in ShopCreateEvents component

--- frontend/src/pages/Shop/ShopCreateEvents.jsx
+++ frontend/src/pages/Shop/ShopCreateEvents.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import DashboardHeader from '../../components/SellerComponents/DashboardHeader'
+import DashboardSidebar from '../../components/SellerComponents/DashboardSidebar'
 import CreateEvent from '../../components/SellerComponents/CreateEvent.jsx'
 
 const ShopCreateEvents = () => {
     return (
         <div>
             <DashboardHeader />
-            <div className="flex items-start justify-between w-full">
-                <div className='w-[80px] width-at-800px'>
+            <div className='flex w-full items-start justify-between'>
+                <div className='w-full max-w-[260px] shrink-0'>
                     <DashboardSidebar activeHeading={6} />
                 </div>
-                <div className="w-full justify-center flex">
+                <div className='flex w-full justify-center'>
                     <CreateEvent />
                 </div>
             </div>
