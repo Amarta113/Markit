dce122c397bcb765dbddf5c8ae63aac37bcc338e
Amarta113 <amartawaghani@gmail.com>
2026-08-12 21:27:54 +0500

    fix: correct state management and variable naming in SuggestedProduct component

--- frontend/src/components/UserComponents/SuggestedProduct.jsx
+++ frontend/src/components/UserComponents/SuggestedProduct.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from 'react'
-import styles from '../../styles/styles'
+import styles from '../../styles/styles.js'
 import ProductCard from '../ProductCard/ProductCard'
 import { useSelector} from 'react-redux'
 
 const SuggestedProduct = ({data}) => {
+    const [products, setProducts] = useState(null)
     const {allProducts} = useSelector((state) => state.products)
-    const [productData, setProductData] = useState(state => state.products)
+
     useEffect(() => {
+        console.log(allProducts)
         const d = allProducts && allProducts.filter((i) => i.category === data.category)
-        setProductData(d)
+        setProducts(d)
     }, [])
+
     return (
         <div>
             {data? (
                 <div className={`p-4 ${styles.section}`}>
                     <h2 className={`${styles.heading} text-[25px] font-[500] border-b mb-5`}>Related Products</h2>
                 <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:gridocols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
-                    {productData && productData.map((i, index) => {
+                    {products && products.map((i, index) => {
                         <ProductCard data={i} key={index}/>
                     })}
                 </div>
