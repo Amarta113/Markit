c1d23056b5ec8add649801e5aa27d90ee8965db6
Amarta113 <amartawaghani@gmail.com>
2026-08-12 18:21:14 +0500

    fix

--- frontend/src/components/ProductDetailsCard/ProductDetailsCard.jsx
+++ frontend/src/components/ProductDetailsCard/ProductDetailsCard.jsx
@@ -41,7 +41,7 @@ const ProductDetailsCard = ({ setOpen, data }) => {
             }
         }
     }
-    const removeFromWishListhandler = (data) => {
+    const removeFromWishlisthandler = (data) => {
         setClick(!click)
         dispatch(removeFromWishlist(data))
     }
@@ -62,7 +62,7 @@ const ProductDetailsCard = ({ setOpen, data }) => {
         <div className='bg-[#fff]'>
             {data ? (
                 <div className='fixed w-full h-screen top-0 left-0 bg-[#00000030] z-40 flex items-center justify-center'>
-                    <div className="w-[90%] md:w-[70%] h-[90vh] overflow-y-scroll 800px:h-[75vh] bg-white rounded-md shadow-sm relative p-4">
+                    <div className="w-[90%] md:w-[70%] h-[90vh] overflow-y-scroll md:h-[75vh] bg-white rounded-md shadow-sm relative p-4">
                         <RxCross1 size={30} className="absolute right-3 top-3 z-50"
                             onClick={() => setOpen(false)}
                         />
@@ -70,7 +70,7 @@ const ProductDetailsCard = ({ setOpen, data }) => {
                             <div className="w-full md:w-[50%]">
                                 <img src={data.image_Url[0].url} alt="" />
                                 <div className='flex'>
-                                    <img src={data.shop.shop_avatar.url} alt=""
+                                    <img src={data?.shop.shop_avatar.url} alt="shop-avatar"
                                         className='w-[50px] h-[50px] rounded-full mr-2'
                                     />
                                     <div>
@@ -121,8 +121,8 @@ const ProductDetailsCard = ({ setOpen, data }) => {
                                             <AiFillHeart
                                                 size={30}
                                                 className="cursor-pointer"
-                                                onClick={() => removeFromWishListhandler(data)}
-                                                color={click ? "red" : "#333"}
+                                                onClick={() => removeFromWishlisthandler(data)}
+                                                color={click ? "red" : "#0e0e0e"}
                                                 title="Remove from wishlist"
                                             />
                                         ) : (
