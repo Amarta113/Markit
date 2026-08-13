d5155ea43255a23b97a98695b6a8a91744d2e387
Amarta113 <amartawaghani@gmail.com>
2026-08-13 22:20:30 +0500

    fix: update Cart component for improved null safety and UI consistency

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import { IoBagHandleOutline } from "react-icons/io5";
 import { HiOutlineMinus, HiPlus } from "react-icons/hi";
 import styles from '../../styles/styles';
 import { useDispatch, useSelector } from 'react-redux';
-import { backend_url } from '../../server';
+import { backend_url } from '../../server.js';
 import { addToCart, removeFromCart } from '../../../redux/actions/cartActions.js';
 import { toast } from 'react-toastify';
 
@@ -30,11 +30,11 @@ export default function Cart({ setOpenCart }) {
                     <div className="w-full h-screen flex items-center justify-center">
                         <div className="flex w-full justify-end pt-5 pr-5 fixed top-3 right-3">
                             <RxCross1 
-                            siz={25}
+                            size={25}
                             className='cursor-pointer'
                             onClick={() => setOpenCart(false)}/>
                         </div>
-                        <h5>Cart Items is empty</h5>
+                        <h5>Cart Items is empty!</h5>
                     </div>
                 ) : (
                     <>
@@ -54,14 +54,14 @@ export default function Cart({ setOpenCart }) {
                             {/* cart single item*/}
                             <br />
                             <div className="w-full border-t">
-                                {cart && cart.map((i, index) => {
-                                    return <CartSingle
+                                {cart && cart?.map((i, index) => (
+                                     <CartSingle
                                         key={index}
                                         data={i}
                                         quantityChangeHandler={quantityChangeHandler}
                                         removeFromCartHandler={removeFromCartHandler}
                                     />
-                                })}
+                                ))}
                             </div>
                         </div>
 
@@ -81,9 +81,9 @@ export default function Cart({ setOpenCart }) {
     )
 }
 
-const CartSingle = ({ data, quantityChangeHandler, removeFromCartHandler }) => {
+const CartSingle = ({ data, i, quantityChangeHandler, removeFromCartHandler }) => {
     const [value, setValue] = useState(data.qty)
-    const totalPrice = data.discountPrice * value;
+    const totalPrice = data?.discountPrice * value;
 
     const increment = (data) => {
         if (data.stock < value) {
@@ -110,18 +110,18 @@ const CartSingle = ({ data, quantityChangeHandler, removeFromCartHandler }) => {
                     >
                         <HiPlus size={18} color='#fff' />
                     </div>
-                    <span className='pl-[10px]'>{data.qty}</span>
+                    <span className='pl-[10px]'>{data?.qty}</span>
                     <div className="bg-[#a7abb14f] rounded-full w-[25px] h-[25px] flex items-center justify-center cursor-pointer"
                         onClick={() => decrement(data)}>
                         <HiOutlineMinus size={16} color='#7d879c' />
                     </div>
                 </div>
-                <img src={`${backend_url}${data?.image[0]}`}
+                <img src={`${data ? data && data?.images[0].url : ""}`}
                     alt="Image"
                     className="w-[130px] h-min ml-2 mr-2 rounded-[5px]" />
                 <div className="pl-[5px]">
-                    <h1>{data.name}</h1>
-                    <h4 className='font-[400] text-[15px] text-[#00000082]'>${data.discountPrice}*{value}</h4>
+                    <h1>{data?.name}</h1>
+                    <h4 className='font-[400] text-[15px] text-[#00000082]'>${data?.discountPrice}*{value}</h4>
                     <h4 className='font-[600] text-[17px] pt-[3px] text-[#d02222] font-Roboto'>US${totalPrice}</h4>
                 </div>
                 <RxCross1 className='cursor-pointer ml-auto' onClick={() => removeFromCartHandler(data)} />
