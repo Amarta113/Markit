1907f0c3f7106a80c538ce2976eeec1626804409
Amarta113 <amartawaghani@gmail.com>
2026-08-13 22:18:33 +0500

    fix: update avatar image source in ProfileContent

--- frontend/src/components/UserComponents/ProfileContent.jsx
+++ frontend/src/components/UserComponents/ProfileContent.jsx
@@ -66,7 +66,7 @@ const ProfileContent = ({ active }) => {
                     <div className="flex flex-col w-full">
                         <div className="flex justify-center pb-8">
                             <div className="relative">
-                                <img src={`${backend_url}${user?.avatar}`}
+                                <img src={`${user?.avatar.url}`}
                                     className='w-[150px] h-[150px] rounded-full object-cover border-[3px] border-[#3ad132]'
                                     alt="profile-img" />
                                 <div className="w-[30px] h-[30px] bg-[#E3E9EE] rounded-full flex items-center justify-center cursor-pointer absolute bottom-[5px] right-[5px]">
