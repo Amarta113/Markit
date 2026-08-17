9759dbb177cf4eb40d5c9b34e2912878dc53df68
Amarta113 <amartawaghani@gmail.com>
2026-08-17 16:38:34 +0500

    update urls

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,5 @@
-export const server = "http://localhost:8000/api/v1"
+export const server = "https://markit-backend-tau.vercel.app/api/v1"
 
-export const backend_url = "http://localhost:8000/"
+export const backend_url = "https://markit-backend-tau.vercel.app/"
+
+export const socket_url = "https://markit-socketio.vercel.app/"
