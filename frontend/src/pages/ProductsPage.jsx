4d87edbd043866879f39ff24bd80ccc3ad8014aa
Amarta113 <amartawaghani@gmail.com>
2026-08-19 15:54:22 +0500

    refactor ProductsPage to improve product filtering and enhance layout with footer

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -4,39 +4,53 @@ import styles from '../styles/styles'
 import { useSearchParams } from 'react-router-dom'
 import ProductCard from '../components/ProductCard/ProductCard'
 import { useSelector } from 'react-redux'
+import Footer from '../components/Layout/Footer'
+import { AiOutlineFire } from 'react-icons/ai'
+import { StretchHorizontal } from 'lucide-react'
 
 export default function ProductsPage() {
     const [searchParams] = useSearchParams()
     const categoryData = searchParams.get("category")
-    const {allProducts} = useSelector((state) => state.products)
+    const {allProducts, isLoading} = useSelector((state) => state.products)
     const [data, setData] = useState([])
 
     useEffect(() => {
-        if (categoryData === null) {
-            const d = allProducts?.sort((a, b) => a.sold_out - b.sold_out)
-            setData(d)
-        } else {
-            const d = allProducts?.filter((i) => i.category === categoryData)
-            setData(d)
+        if(categoryData === null){
+            const data = allProducts
+            setData(data)
+        }else {
+            const data = allProducts && allProducts.filter(i => i.category === categoryData)
+            setData(data)
         }
-        //window.scrollTo(0,0)
-    }, [allProducts])
+    }, [categoryData, allProducts])
 
     return (
         <div className="text-black">
             <Header activeHeading={3} />
+            <div className="border-b border-[#00000012] bg-gradient-to-b bg-[#fff] text-center">
+                <div className={`${styles.section} py-10`}>
+                    <h1 className="text-[28px] md:text-[34px] text-[#1a1a1a]">
+                        Check All Products
+                    </h1>
+                </div>
+            </div>
             <br />
-            <br />
-            <div className={`${styles.section}`}>
+            <div className={`${styles.section} py-8`}>
                 <div className="grid grid-cols-1 gap-[20px] md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
                     {
-                        data?.map((i, index) => <ProductCard data={i} key={index} />)
+                       data && data?.map((i, index) => <ProductCard data={i} key={index} />)
                     }
                 </div>
                 {
-                    data?.length === 0 ? (<h1 className='text-center w-full pb-[10px] text-[20px]'>No products found!</h1>) : null
+                    data?.length === 0 ? (
+                    <div className="">
+                        <StretchHorizontal size={28} /> 
+                        <h1 className='text-center w-full pb-[10px] text-[20px]'>No products found!</h1>
+                    </div>
+                    ) : null
                 }
             </div>
+            <Footer/>
         </div>
     )
 }
